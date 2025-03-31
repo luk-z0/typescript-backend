a1b5406805b01a7ce82f765f0345954e983280fe
Lucas Gabriel <lucas1020111940@hotmail.com>
2025-03-31 18:37:59 -0300

    add controller

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import dotenv from "dotenv";
 dotenv.config()
 
 import './connection';
+import productController from "./controllers/product.controllers";
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
+
+app.get('/api/products', productController.findAll);
 
 app.get('/', (request, response) => {
   response.send("SERVER UP!")
