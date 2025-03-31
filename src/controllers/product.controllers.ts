10e6453867ef1237d0c94bd60228b872f94dbd1d
Lucas Gabriel <lucas1020111940@hotmail.com>
2025-03-31 19:23:18 -0300

    create products

--- src/controllers/product.controllers.ts
+++ src/controllers/product.controllers.ts
@@ -1,16 +1,38 @@
 import { Request, Response } from 'express'
 import AppDataSource from '../connection'
 import { Product } from '../entities/product.entity'
+import { Repository } from 'typeorm';
 
 class ProductController {
-  async findAll(request: Request, response: Response): Promise<Response> {
+  // private productRepository: Repository<Product>
 
+  // constructor() {
+  //   this.productRepository = AppDataSource.getRepository(Product);
+  // }
+
+  async findAll(request: Request, response: Response): Promise<Response> {
     const productRepository = AppDataSource.getRepository(Product);
 
     const products = await productRepository.find()
 
     return response.status(200).send({
       data: products
+    })
+  }
+
+  async create(request: Request, response: Response): Promise<Response> {
+    const { name, description, weight } = request.body;
+
+    const productRepository = AppDataSource.getRepository(Product);
+
+    const product = new Product
+    product.name = name
+    product.weight = weight
+    product.description = description
+    const newProduct = await productRepository.create(product)
+
+    return response.status(201).send({
+      data: newProduct
     })
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import bodyParser from "body-parser";
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config()
@@ -8,8 +9,11 @@ import productController from "./controllers/product.controllers";
 const PORT = process.env.PORT || 8080
 
 const app = express()
+app.use(bodyParser.json());
 
 app.get('/api/products', productController.findAll);
+
+app.post('/api/products', productController.create);
 
 app.get('/', (request, response) => {
   response.send("SERVER UP!")
